4d221117509757be91d27aae58dacdba637bbfe4
Alfer <birukov1andrey@gmail.com>
2021-09-27 19:09:41 +0300

    Lab1
    Lab1

---
+++ com2com(Lab_1)/com2com/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace com2com
+{
+    partial class com2com
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.ComboBox = new System.Windows.Forms.ComboBox();
+            this.Debug = new System.Windows.Forms.Label();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.OutputBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // InputBox
+            // 
+            this.InputBox.Location = new System.Drawing.Point(12, 12);
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(288, 22);
+            this.InputBox.TabIndex = 0;
+            // 
+            // ComboBox
+            // 
+            this.ComboBox.FormattingEnabled = true;
+            this.ComboBox.Location = new System.Drawing.Point(179, 287);
+            this.ComboBox.Name = "ComboBox";
+            this.ComboBox.Size = new System.Drawing.Size(121, 24);
+            this.ComboBox.TabIndex = 1;
+            this.ComboBox.SelectedIndexChanged += new System.EventHandler(this.ComboBox_SelectedIndexChanged_1);
+            // 
+            // Debug
+            // 
+            this.Debug.AutoSize = true;
+            this.Debug.Location = new System.Drawing.Point(12, 258);
+            this.Debug.Name = "Debug";
+            this.Debug.Size = new System.Drawing.Size(20, 17);
+            this.Debug.TabIndex = 2;
+            this.Debug.Text = "...";
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(179, 258);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(121, 23);
+            this.SendButton.TabIndex = 3;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.FormattingEnabled = true;
+            this.OutputBox.ItemHeight = 16;
+            this.OutputBox.Location = new System.Drawing.Point(12, 108);
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.Size = new System.Drawing.Size(288, 132);
+            this.OutputBox.TabIndex = 4;
+            // 
+            // com2com
+            // 
+            this.ClientSize = new System.Drawing.Size(312, 376);
+            this.Controls.Add(this.OutputBox);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.Debug);
+            this.Controls.Add(this.ComboBox);
+            this.Controls.Add(this.InputBox);
+            this.Name = "com2com";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Com2com_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Debug;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.ComboBox ComboBox;
+        private System.Windows.Forms.ListBox OutputBox;
+        private System.Windows.Forms.TextBox InputBox;
+    }
+}
+
---
+++ com2com(Lab_1)/com2com/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+
+namespace com2com
+{
+    public partial class com2com : Form
+    {
+
+        SerialPort comPort;
+        Thread readThread;
+        String portName;
+        bool flg = true;
+        public com2com()
+        {
+            InitializeComponent();
+            this.FormClosing += Com2com_FormClosing;
+
+            string[] ports = SerialPort.GetPortNames();
+            Debug.Text = "";
+            for (int i = 0; i < ports.Length; i++)                   // Add Ports names in ComboBox
+            {
+                Debug.Text += "i = " + i + "  " + ports[i] + "\n";
+                ComboBox.Items.Add(ports[i]);
+            }
+            for (int i = 0; i < ports.Length; i++)                  //Choose first port 
+            {
+                ComboBox.SelectedIndex = i;
+                comPort = new SerialPort(ports[i]);
+                if (comPort.IsOpen == false)
+                {
+                    try
+                    {
+                        portName = ComboBox.SelectedItem.ToString();
+                        comPort.Open();
+                    }catch (UnauthorizedAccessException) { continue; }
+                    i = ports.Length + 1;
+                    break;
+                }
+            }
+            comPort.ReadTimeout = 1000;
+            readThread = new Thread(read);
+            readThread.Start();
+        }
+
+        private void read(){                                        // Thread for check new message in port
+            while (readThread.ThreadState.ToString() != "WaitSleepJoin" && readThread.ThreadState.ToString() != "Aborted")
+            {
+                Console.WriteLine("..."+readThread.ThreadState.ToString()+ "...");
+                try{
+                    if (comPort.IsOpen){
+                        Console.WriteLine("comport is open\n");
+                        string portname = comPort.PortName;
+                        string readLine = comPort.ReadLine();
+                        OutputBox.Invoke((MethodInvoker)delegate { OutputBox.Items.Add(readLine); });
+                    }
+                }
+                catch (TimeoutException e) { Console.WriteLine("TimeoutException -... " + e.Message); }
+                catch (ObjectDisposedException ) {
+                    //System.Threading.Timer t = new System.Threading.Timer(null, null, 1000, 1);
+                    Console.WriteLine("ObjectDisposedException \n");
+                    Thread.Sleep(1000);
+                    flg = false;
+                }
+                //catch (InvalidOperationException IOE) { Console.WriteLine("InvalidOperationException (Port closed) - " + IOE.Message); }
+                
+                while(flg == false){
+                    Console.WriteLine("flg == false\n");
+                    Thread.Sleep(100000);
+                    flg = true;
+                }
+            }
+        }
+        private void SendButton_Click(object sender, EventArgs e){
+            string writeLine = Convert.ToString(InputBox.Text);
+            comPort.WriteLine(writeLine);
+            InputBox.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e){
+        }
+
+        private void OutputBox_SelectedIndexChanged(object sender, EventArgs e){
+        }
+
+        private void comboBox_SelectedIndexChanged(object sender, EventArgs e){
+        }
+
+        public void Com2com_FormClosing(object sender, FormClosingEventArgs e){
+            readThread.Abort();
+            comPort.Close();
+            //Application.Exit();
+            System.Environment.Exit(0);
+            System.Environment.FailFast("Exit Error!");
+        }
+
+        //portName - хранит текущий порт, tmp_port - старый порт 
+        //PortName сохраняет имя нового порта, отключаемся от старого порта
+        //и пытемся подключится к новому. если не получается, то подключаемся к старому
+        private void ComboBox_SelectedIndexChanged_1(object sender, EventArgs e){
+            string tmp_name = portName;
+            portName = ComboBox.SelectedItem.ToString();
+            if (comPort != null){
+                try
+                {
+                    Console.WriteLine(readThread.ThreadState.ToString());
+                    readThread.Abort();
+                    Console.WriteLine(readThread.ThreadState.ToString());
+                    //flg = false;
+                    Thread.Sleep(10000);
+
+                    readThread.Interrupt();
+                    Console.WriteLine(readThread.IsAlive.ToString());
+
+                    comPort.Close();
+                    comPort = new SerialPort(portName);
+                    comPort.Open();
+                    //readThread.Abort();
+                    readThread = new Thread(read);
+                    Console.WriteLine(readThread.ThreadState.ToString());
+                    readThread.Start();
+                    Console.WriteLine(readThread.ThreadState.ToString());
+                    //flg = true;
+
+                }
+                catch (System.Threading.ThreadAbortException exception) {
+                    Console.WriteLine("Abort exception - " + exception.Message);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    comPort.Close();
+                    comPort = new SerialPort(tmp_name);
+                    comPort.Open();
+                    //readThread = new Thread(read);
+                    readThread.Start();
+                    ComboBox.SelectedItem = tmp_name;
+                    Debug.Text = "Com Port is Closed. \nConnect to " + tmp_name;
+                }
+            }
+        }
+    }
+}
