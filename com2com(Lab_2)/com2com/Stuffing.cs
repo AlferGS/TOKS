b0e5ec8f06b7811bce373a8906f40723694acc68
Alfer <birukov1andrey@gmail.com>
2021-10-31 22:59:20 +0300

    Update 12
    Lab2

--- com2com(Lab_2)/com2com/Stuffing.cs
+++ com2com(Lab_2)/com2com/Stuffing.cs
@@ -10,52 +10,102 @@ namespace com2com
 {
     public partial class Stuffing: Form
     {
-        private const string flag = "b";
-        private const string esc  = "#";
-        private const string changedEsc = "&";
+        private const char flag = 'b';
+        private const char Esc = '@';
+        private const char plusEsc  = '#';
+        private const char notEsc = '&';
 
         public string ByteStuffing(string message, RichTextBox Debug)
         {
-            message = message.Replace(esc, changedEsc);
-            message = message.Replace(flag, "@" + esc);
-            for (int index = 0; index < message.Length; index++)
+            string tempString = "";
+            for (int i = 0; i < message.Length; i++) 
             {
-                string output = "";
-                if ((message[index] == '@') && (message[index + 1] == esc[0]))
+                if (message[i] == Esc)              // Find esc symbols
                 {
-                    output = "@"+ esc;
-                    Debug.SelectionColor = Color.Red;
-                    Debug.AppendText(output);
+                    Debug.SelectionStart = Debug.Text.Length;
+                    tempString += "" + Esc + notEsc;
+                    Debug.SelectionColor = Color.Green;
+                    Debug.AppendText("" +Esc + notEsc);
                     Debug.SelectionColor = Color.Black;
-                    output = "";
-                    index++;
+                }
+                else if (message[i] == flag)        // Find flag symbols
+                {
+                    Debug.SelectionStart = Debug.Text.Length;
+                    tempString += ""+ Esc + plusEsc;
+                    Debug.SelectionColor = Color.Red;
+                    Debug.AppendText("" + Esc + plusEsc);
+                    Debug.SelectionColor = Color.Black;
                 }
                 else
                 {
-                    if ((message[index] == changedEsc[0]))
-                    {
-                        output = changedEsc;
-                        Debug.SelectionColor = Color.Green;
-                        Debug.AppendText(output);
-                        Debug.SelectionColor = Color.Black;
-                        output = "";
-                        index++;
-                    }
-                    else
-                    {
-                        output = "";
-                        output += message[index];
-                        Debug.AppendText(output);
-                    }
+                    tempString += message[i];
+                    Debug.AppendText(message[i].ToString());
                 }
             }
-            return message;
+            return tempString;
         }
         public string UnByteStuffing(string message)
         {
-            message = message.Replace("@" + esc, flag);
-            message = message.Replace(changedEsc, esc);
-            return message;
+            string tempString = "";
+            for(int i = 0; i< message.Length; i++)
+            {
+                if (message[i] == Esc && message[i+1] == plusEsc)    // Back flag
+                {
+                    i++;
+                    tempString += flag;
+                }
+                else if (message[i] == Esc && message[i+1] == notEsc)   //Find notEsc
+                {
+                    i++;
+                    tempString += Esc;
+                }
+                else
+                {
+                    tempString += message[i];
+                }
+            }
+            return tempString;
         }
     }
 }
+
+            //message = message.Replace("@" + esc, flag);
+            //message = message.Replace(changedEsc, esc);
+            //message = message.Replace(esc, changedEsc);
+            //message = message.Replace(flag, "@" + esc);
+
+
+
+        //    for (int index = 0; index < message.Length; index++)
+        //    {
+        //        string output = "";
+        //        if ((message[index] == '@') && (message[index + 1] == plusEsc))
+        //        {
+        //            //output += Esc + plusEsc;
+        //            Debug.SelectionColor = Color.Red;
+        //            Debug.AppendText(output);
+        //            Debug.SelectionColor = Color.Black;
+        //            output = "";
+        //            index++;
+        //        }
+        //        else
+        //        {
+        //            if ((message[index] == notEsc))
+        //            {
+        //                output += notEsc;
+        //                Debug.SelectionColor = Color.Green;
+        //                Debug.AppendText(output);
+        //                Debug.SelectionColor = Color.Black;
+        //                output = "";
+        //                index++;
+        //            }
+        //            else
+        //            {
+        //                output = "";
+        //                output += message[index];
+        //                Debug.AppendText(output);
+        //            }
+        //        }
+        //    }
+        //    return message;
+        //}
